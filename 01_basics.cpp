9a67b85aeb7f92af35f2db5157293b27c144365e
Manoj Kumar <contact@manojkumar-dev.com>
2026-06-22 17:18:41 +0000

    01_basics chapter done.

---
+++ 01_basics.cpp
@@ -0,0 +1,35 @@
+// First C++ Program
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Mnaoj" << endl;
+    return 0;
+}
+
+
+
+// Single line comment
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Print command
+    cout << "Mnaoj" << endl;
+    return 0;
+}
+
+
+
+// Multi-line comment
+#include <iostream>
+using namespace std;
+
+int main(){
+    /* This is a
+    multi-line comment */
+    cout << "Mnaoj" << endl;
+    return 0;
+}
+
